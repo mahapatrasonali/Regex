0338c38112753993c41ec31e9a66ff6e7261fb7a
[mahapatrasonali] <[sonangr2000@gmail.com]>
2023-01-03 19:31:33 +0530

    First Name is valid or Not

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace RegexUserRegistration
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Your FirstName: ");
+            string username = Console.ReadLine();
+            UserName userName = new UserName();
+            bool name = userName.validateUserName(username);
+            if (name)
+            {
+                Console.WriteLine("UserName is Valid");
+            }
+            else
+            {
+                Console.WriteLine("UserName is not Valid");
+            }
+        }
+    }
+}
