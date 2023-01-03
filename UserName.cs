6ae14f4d3062016505e5219853d1e7928913431e
[mahapatrasonali] <[sonangr2000@gmail.com]>
2023-01-03 19:32:18 +0530

    FirstName

---
+++ UserName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexUserRegistration
+{
+    public class UserName
+    {
+        public static string REGEX_USERNAME = "^[A-Z]{1}[a-z]{2}$";
+
+        public bool validateUserName(string userName)
+        {
+            return Regex.IsMatch(userName, REGEX_USERNAME);
+        }
+    }
+}
